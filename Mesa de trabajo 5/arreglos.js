d5a48b0cbafb9ad68abf97206963137ad2409c8f
Silvana Breton <silbreton@hotmail.com>
2024-08-22 21:10:46 -0300

    nuevos ejercicios

---
+++ Mesa de trabajo 5/arreglos.js
@@ -0,0 +1,10 @@
+//Acceder a elementos específicos de un array.
+
+let colores = ["azul", "rojo", "verde", "blanco", "negro"];
+console.log(colores[4]);
+
+//Modificar cada uno de sus elementos e imprimirlos.
+
+let otroColor = colores.push("amarillo");
+console.log(otroColor);
+console.log(colores);
